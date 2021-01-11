a21b15ceb873a210ba2ca3c375db547f1e5a6a7b
Sevz17 <leohdz172@outlook.com>
2021-01-10 21:15:01 -0600

    Time: Added seconds to the hour

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,9 +3,8 @@ static const Block blocks[] = {
 	{"Mem:", "free --mega | awk '/^Mem/ { print $3\"M/\"$2\"M\" }'",	1,		0},
     { "  ", "checkupdates | wc -l",                 60,               0 },
     { "",    "volume",                               1,                0 },
-    { "",    "date '+ %d/%m/%Y  %H:%M'",       1,              0 },
+    { "",    "date '+ %d/%m/%Y  %H:%M:%S'",       1,              0 },
 };
-//free --mega -s 1 -c 10
 
 // Sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = "  ";
